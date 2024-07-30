742a49374dc6c988aef91fdc5e306bc8aed092be
hakhal6 <98768102+hajar6102@users.noreply.github.com>
2024-07-30 10:55:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const myModal = document.getElementById('.modal');
+//     const myInput = document.querySelector('.modal-input');
+
+//     myModal.addEventListener('shown.bs.modal', () => {
+//         myInput.focus();
+//     });
+// });
